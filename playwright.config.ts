26bc796cf05470497b30785b5ad4685f393543fc
ialwahaibi <ialwahaibi@projectx.om>
2026-04-26 01:58:51 +0400

    added multi-browser suppoer

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+  baseURL: 'https://www.saucedemo.com',
+  trace: 'retain-on-failure',
+  screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+},
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
 
   /* Configure projects for major browsers */
   projects: [
